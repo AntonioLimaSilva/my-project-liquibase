755fd48d83f0eaab9765e2a247715eb051743c44
AntonioLimaSilva <luclimasilva@hotmail.com>
2019-06-16 21:30:14 -0300

    Initial commit

---
+++ src/main/java/br/com/luciano/myprojectliquibase/MyProjectLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.luciano.myprojectliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectLiquibaseApplication.class, args);
+	}
+
+}
