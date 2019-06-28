12cf7a48fbc61c0f27cc710b586c6a4bbfd46a06
AntonioLimaSilva <luclimasilva@hotmail.com>
2019-06-28 00:11:25 -0300

    using save all

--- src/main/java/br/com/luciano/myprojectliquibase/MyProjectLiquibaseApplication.java
+++ src/main/java/br/com/luciano/myprojectliquibase/MyProjectLiquibaseApplication.java
@@ -1,13 +1,26 @@
 package br.com.luciano.myprojectliquibase;
 
+import br.com.luciano.myprojectliquibase.model.User;
+import br.com.luciano.myprojectliquibase.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyProjectLiquibaseApplication {
+public class MyProjectLiquibaseApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectLiquibaseApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+        Iterable<User> all = this.userRepository.findAll();
+
+        this.userRepository.saveAll(all);
+    }
 }
--- src/main/java/br/com/luciano/myprojectliquibase/model/User.java
+++ src/main/java/br/com/luciano/myprojectliquibase/model/User.java
@@ -1,8 +1,13 @@
 package br.com.luciano.myprojectliquibase.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -10,62 +15,10 @@ public class User {
     @Id
     @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @EqualsAndHashCode.Include
     private Integer id;
     private String name;
     private String email;
     private String password;
     private Boolean active;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return Objects.equals(getId(), user.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
